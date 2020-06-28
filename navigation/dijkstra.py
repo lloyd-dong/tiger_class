2bde344d45d4cdfdd006227c046d9f03e478dde4
Bo Dong <bo_dong@apple.com>
2020-06-28 16:51:47 +0800

    finished dijkstra with debug info

--- navigation/dijkstra.py
+++ navigation/dijkstra.py
@@ -7,6 +7,15 @@ road = {
 }
 
 
+# road = {
+#     1: ((2, 50), (3, 45),(4,10)),
+#     2: ((4, 15), (3, 10)),
+#     3: ((5, 30),),
+#     4: ((1,10), (5, 15),),
+#     5: ((2, 20), (3,35)),
+#     6: ((5,3),)
+# }
+
 class Route:
     def __init__(self, start):
         self.start = start
@@ -14,24 +23,15 @@ class Route:
         self.path = [start]
         self.cost = 0
 
-    def copy(self):
-        new_one = Route(self.start)
-        new_one.end = self.end
-        new_one.cost = self.cost
-        # python 2
-        # new_one.path = []
-        # new_one.path.extend(self.path)
-        new_one.path = self.path.copy()
-        return new_one
-
     def __str__(self):
         return "cost: {}, path: {}".format(self.cost, self.path)
 
     def move_forward(self, _edge):
-        _r = self.copy()
+        _r = Route(self.start)
         _r.end = _edge[0]
+        _r.path = self.path.copy()
         _r.path.append(_edge[0])
-        _r.cost += _edge[1]
+        _r.cost = self.cost + _edge[1]
         return _r
 
 
@@ -41,39 +41,40 @@ def find_route(start, end, routs):
             return rout
     return False
 
-def sort_set(routes):
-    l = []
-    l.extend(routes)
-    l.sort(key=lambda x: x.cost)
-    return l
-
 s = 1
 e = 6
-routes = {Route(s)}
-found = False
-while not found:
-    del_set = set()
-    add_set = set()
-    sorted_routs = sort_set(routes)
-    previous_route_cost = float('inf')
-    for r in sorted_routs:
-        if r in del_set or r.cost > previous_route_cost:
+r = Route(s)
+routes = {r}
+iterated_points = set()
+is_over = False
+
+while not is_over:
+    print("-------------")
+    print("\n".join([str(sr) for sr in routes]))
+
+    routes.remove(r)
+    iterated_points.add(r.end)
+    edges = road[r.end]
+    for edge in edges:
+        if edge[0] in iterated_points:
+            print("{} already visited for Route {}".format(edge[0], r))
             continue
-        previous_route_cost = r.cost
-        del_set.add(r)
-        edges = road[r.end]
-        for edge in edges:
-            r1 = r.move_forward(edge)
-            r0 = find_route(r1.start, r1.end, routes)
-            if not r0:
-                add_set.add(r1)
-            elif r0.cost > r1.cost:
-                del_set.add(r0)
-                add_set.add(r1)
+        r1 = r.move_forward(edge)
+        if edge[0] == e:
+            print("found one {}".format(r1))
+        r0 = find_route(r1.start, r1.end, routes)
+        if not r0:
+            routes.add(r1)
+        elif r0.cost > r1.cost:
+            print("{} has been replaced by {} during relaxation".format(r0, r1))
+            routes.remove(r0)
+            routes.add(r1)
+        else:
+            print("{} was dropped during relaxation".format(r1))
+    if len(routes) > 0:
+        r = min(routes, key=lambda x: x.cost)
+    is_over = len(routes) == 0 or e == r.end
 
-    routes.difference_update(del_set)
-    routes.update(add_set)
-
-    found = all({r.end == e for r in routes}) and len({r.cost for r in routes}) == 1
-print(len(routes))
-print(routes.pop())
+print("------ The end ------")
+if len(routes) > 0:
+    print("shortest route {}".format(r))

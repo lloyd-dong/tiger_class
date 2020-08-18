f7c995dbbcc10dbf354a04164938a5a33e3eec73
Bo Dong <bo_dong@apple.com>
2020-08-18 21:01:37 +0800

    calculate cola guess ratio

---
+++ learn_python/cola_calc.py
@@ -0,0 +1,14 @@
+from scipy.special import comb
+import numpy as np
+
+bottles_of_cola = 10
+total = pow(2,bottles_of_cola)
+
+data = []
+for i in range(bottles_of_cola+1):
+	data.append(comb(bottles_of_cola,i))
+pdf = np.around(np.divide(data, total),4)
+cdf = np.around(np.cumsum(pdf),3)
+print("sum: {}, data: {}".format(sum(data),data))
+print("pdf: {}".format(pdf))
+print("cdf: {}%".format(cdf))
